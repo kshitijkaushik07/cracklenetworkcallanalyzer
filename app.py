70e3e34cca12f59df9bd59834d0a27527d25a336
kshitijkaushik07 <kshitijkaushik@crackle.tech>
2025-01-12 18:33:50 +0530

    Final code push

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ app.secret_key = 'your_secret_key'
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
 def har_to_csv():
-    # Full path for HAR and CSV files
     har_file_name = 'upload_har.har'
     har_folder = 'har_file_upload'
     csv_folder = 'uploads'
@@ -30,15 +29,12 @@ def har_to_csv():
     csv_file_path = os.path.join(csv_folder, csv_file_name)
 
     try:
-        # Open HAR file and load the data
         with open(har_file_path, "r", encoding="utf-8") as file:
             har_data = json.load(file)
 
-        # Open CSV file for writing
         with open(csv_file_path, "w", newline='', encoding="utf-8") as csvfile:
             csv_writer = csv.writer(csvfile)
 
-            # Write header row for CSV
             csv_writer.writerow([
                 "Request URL",
                 "Request Method",
@@ -51,7 +47,6 @@ def har_to_csv():
                 "Ping (Wait Time)"
             ])
 
-            # Parse the HAR entries and write to CSV
             for entry in har_data.get('log', {}).get('entries', []):
                 request = entry.get('request', {})
                 response = entry.get('response', {})
@@ -210,16 +205,16 @@ def index():
         ]
     }
 
-    # Path to your uploaded CSV file
+    
     UPLOAD_FOLDER = 'uploads'
     file_name = 'uploaded_file.csv'
     file_path = os.path.join(UPLOAD_FOLDER, file_name)
 
-    # Arrays for 'iu' values
+    
     iu_with_crackle = []
     iu_without_crackle = []
 
-    # If the file exists, proceed with reading and processing the CSV
+
     if os.path.exists(file_path):
         df = pd.read_csv(file_path)
 
@@ -237,10 +232,7 @@ def index():
                         aggregated_flags[sspAndPingType].append(False)
 
         print(aggregated_flags['GAM_init'])
-        # Call the function for response body processing
         process_response_body(df, iu_with_crackle, iu_without_crackle)
-
-        # Print results
         print("Aggregated Flags: ")
         for flag, value in aggregated_flags.items():
             print(f"{flag}: {value}")
@@ -250,39 +242,31 @@ def index():
     else:
         print(f"File '{file_name}' not found in folder '{UPLOAD_FOLDER}'.")
 
-    # Render the results to the template
     return render_template('new_index.html', flags=aggregated_flags,
                            iu_with_crackle=iu_with_crackle, iu_without_crackle=iu_without_crackle)
 
 
     
 def process_response_body(df, iu_with_crackle, iu_without_crackle):
-    # Traverse each row in the DataFrame
     for index, row in df.iterrows():
         request_url = row.get('Request URL', '')
         
-        # Proceed only if the 'Request URL' contains the specified expression
         if 'pubads.g.doubleclick.net/gampad/ads' in request_url:
             encoded_response_body = row.get('Response Body', '')
             
-            # Fix padding for the base64 string
             missing_padding = len(encoded_response_body) % 4
             if missing_padding != 0:
                 encoded_response_body += "=" * (4 - missing_padding)
             
-            # Decode the base64 string
             try:
                 decoded_bytes = base64.b64decode(encoded_response_body)
-                decoded_response_body = decoded_bytes.decode('utf-8')  # Assuming UTF-8 encoded
+                decoded_response_body = decoded_bytes.decode('utf-8')
                 
-                # Parse the decoded response body as JSON
                 response_json = json.loads(decoded_response_body)
                 
-                # Check if "ad_networks" and "class_name" exist in the JSON
                 for ad_network in response_json.get('ad_networks', []):
                     class_name = ad_network.get('data', {}).get('class_name', '').lower()
                     
-                    # Extract 'iu' values from the Request URL's query parameters
                     parsed_url = urlparse(request_url)
                     query_params = parse_qs(parsed_url.query)
                     iu_values_list = query_params.get('iu', [])
@@ -303,39 +287,19 @@ def process_response_body(df, iu_with_crackle, iu_without_crackle):
 def calculate_class_size(class_obj):
     total_size = 0
 
-    # Size of the class name
     total_size += len(class_obj.get_name().encode('utf-8'))
 
-    # # Size of the superclass name
-    # if class_obj.get_superclassname():
-    #     total_size += len(class_obj.get_superclassname().encode('utf-8'))
-
-    # Size of implemented interfaces
     for interface in class_obj.get_interfaces():
         total_size += len(interface.encode('utf-8'))
 
-    # Size of annotations
-    # annotations = class_obj.get_annotations()
-    # if annotations:
-    #     for annotation in annotations:
-    #         total_size += len(str(annotation).encode('utf-8'))
-
-    # Size of methods
     for method in class_obj.get_methods():
         if method.get_code():
             total_size += method.get_code().get_length()
-        total_size += len(method.get_name().encode('utf-8'))  # Add the size of the method name
-        total_size += len(method.get_descriptor().encode('utf-8'))  # Add method descriptor size
-
-    # Size of fields
+        total_size += len(method.get_name().encode('utf-8'))  
+        total_size += len(method.get_descriptor().encode('utf-8'))  
     for field in class_obj.get_fields():
         total_size += len(field.get_name().encode('utf-8'))
-        total_size += len(field.get_descriptor().encode('utf-8'))  # Add field descriptor size
-
-    # Add shared references (strings, constants, etc.)
-    # for reference in analysis.get_field_analysis(class_obj):
-    #     total_size += len(reference.field.get_name())
-    #     total_size += len(reference.field.get_descriptor())
+        total_size += len(field.get_descriptor().encode('utf-8'))  
 
     return total_size
 
@@ -348,48 +312,82 @@ def get_class_size_from_dex(dex_content, target_class_name):
     return total_size
 
 @app.route('/uploadApk', methods=['POST'])
-def upload_apk():
-    # Check if the 'apkFile' is part of the request
-    print("1")
+def upload_apk():    
     if 'apkFile' not in request.files:
         return jsonify({"message": "No file part in the request"}), 400
-    print("2")
+   
     file = request.files['apkFile']
 
-    # Check if a file was selected
-    print("3")
+    
     if file.filename == '':
         return jsonify({"message": "No file selected for uploading"}), 400
 
-    # Ensure the file is an APK
-    print("4")
     if not file.filename.endswith('.apk'):
         return jsonify({"message": "Invalid file type. Only .apk files are allowed."}), 400
 
-    # Save the uploaded file
-    print("5")
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
     file.save(file_path)
 
     target_ssps = {
-        'Lcom/google/android/gms': 0.0,
-        'Lcom/applovin': 0.0,
-        'Lcom/ironsource':0.0,
-        'Lcom/fyber': 0.0,
-        'Lcom/facebook':0.0,
-        'Lcom/adinmo':0.0,
-        'Lcom/admofi':0.0,
-        'Lcom/unity':0.0,
-        'Lcom/gadsme':0.0,
-        'Lcom/admob':0.0,
+        # Inmobi
         'Lcom/inmobi':0.0,
-        'Lcom/vungle':0.0,
+        'Inmobi':0.0,
+
+        # Adster
         'Lcom/adster':0.0,
-        'Lcom/flury':0.0,
+        'Adster':0.0,
+
+        # IronSource
+        'Lcom/ironsource':0.0,
+        'Lcom/iab/omid/library/ironsrc':0.0,
+        'Lcom/unity3d/ironsourceads':0.0,
+        'Lcom/unity3d/mediation':0.0,
+        'IronSource':0.0,
+
+        # GAM 
+        'Lcom/google/android/gms':0.0,
+        'GAM/Admob':0.0,
+        
+        # Mintegral
+        'Lcom/mbridge/msdk':0.0,
+        'Lcom/iab/omid/library/mmadbridge':0.0,
+        'Mintegral':0.0,
+
+        # Applovin
+        'Lcom/applovin':0.0,
+        'Lcom/iab/omid/library/applovin':0.0,
+        'Applovin':0.0,
+
+        # Unity_Clouds
+        'Lcom/unity3d/ads':0.0,
+        'Lcom/unity3d/services':0.0,
+        'Lcom/iab/omid/library/unity3d':0.0,
+        'Lcom/unity3d/scar/adapter/v2000':0.0,
+        'Lcom/unity3d/scar/adapter/v2100':0.0,
+        'Lcom/unity3d/scar/adapter/v2300':0.0,
+        'Lcom/unity3d/scar/adapter/common':0.0,
+        'Unity_Clouds':0.0,
+        
+        # vungle
+        'Lcom/iab/omid/library/vungle': 0.0,
+        'Lcom/vungle/ads': 0.0,
+        'vungle':0.0,
+        
+        # facebook
+        'Lcom/facebook/ads':0.0,
+        'Lcom/facebook/infer/annotation':0.0,
+        'facebook':0.0,
+
+        # DigitalTurbine/Fyber
+        'Lcom/digitalturbine/ignite':0.0,
+        'Lcom/fyber/inneractive':0.0,
+        'Lcom/fyber/marketplace':0.0,
+        'Lcom/iab/omid/library/fyber':0.0,
+        'DigitalTurbine/Fyber':0.0,
+
     }
 
-    print("5")
     with ZipFile(file_path, 'r') as apk_zip:
         for target_ssp in target_ssps.keys():
             for dex_file in apk_zip.namelist():
@@ -400,15 +398,105 @@ def upload_apk():
                         dex_content = dex_file_content.read()
                         new_size = get_class_size_from_dex(dex_content, target_ssp)
                         target_ssps[target_ssp] = target_ssps[target_ssp] + (new_size / (1024 * 1024))  
+    
+    print(target_ssps['Lcom/iab/omid/library/vungle'],target_ssps['Lcom/vungle/ads'])
 
-    print("6")
+    # Inmobi 
+    inmobi_value = target_ssps.get('Lcom/inmobi', 0)
+    target_ssps['Inmobi'] = inmobi_value
+    target_ssps.pop('Lcom/inmobi', None)
+
+    # Adster
+    adster_value = target_ssps.get('Lcom/adster', 0)
+    target_ssps['Adster'] = adster_value
+    target_ssps.pop('Lcom/adster', None)
+
+    # iron source
+    ironsource_value = target_ssps.get('Lcom/ironsource', 0)
+    omid_value = target_ssps.get('Lcom/iab/omid/library/ironsrc', 0)
+    unity_ads_value = target_ssps.get('Lcom/unity3d/ironsourceads', 0)
+    unity_mediation_value = target_ssps.get('Lcom/unity3d/mediation', 0)
+    target_ssps['IronSource'] = (
+    ironsource_value + omid_value + unity_ads_value + unity_mediation_value
+    )
+    target_ssps.pop('Lcom/ironsource', None)
+    target_ssps.pop('Lcom/iab/omid/library/ironsrc', None)
+    target_ssps.pop('Lcom/unity3d/ironsourceads', None)
+    target_ssps.pop('Lcom/unity3d/mediation', None)
+
+    # Mintegral 
+    mbridge_value = target_ssps.get('Lcom/mbridge/msdk', 0)
+    omid_value = target_ssps.get('Lcom/iab/omid/library/mmadbridge', 0)
+    target_ssps['Mintegral'] = mbridge_value + omid_value
+    target_ssps.pop('Lcom/mbridge/msdk', None)
+    target_ssps.pop('Lcom/iab/omid/library/mmadbridge', None)
+
+    # Applovin
+    applovin_value = target_ssps.get('Lcom/applovin', 0)
+    omid_value = target_ssps.get('Lcom/iab/omid/library/applovin', 0)
+    target_ssps['Applovin'] = applovin_value + omid_value
+    target_ssps.pop('Lcom/applovin', None)
+    target_ssps.pop('Lcom/iab/omid/library/applovin', None)
+
+    # GAM/Admob
+    google_gms_value = target_ssps.get('Lcom/google/android/gms', 0)
+    target_ssps['GAM/Admob'] = google_gms_value
+    target_ssps.pop('Lcom/google/android/gms', None)
+
+    # Unity_clouds 
+    ads_value = target_ssps.get('Lcom/unity3d/ads', 0)
+    services_value = target_ssps.get('Lcom/unity3d/services', 0)
+    omid_value = target_ssps.get('Lcom/iab/omid/library/unity3d', 0)
+    scar_v2000_value = target_ssps.get('Lcom/unity3d/scar/adapter/v2000', 0)
+    scar_v2100_value = target_ssps.get('Lcom/unity3d/scar/adapter/v2100', 0)
+    scar_v2300_value = target_ssps.get('Lcom/unity3d/scar/adapter/v2300', 0)
+    scar_common_value = target_ssps.get('Lcom/unity3d/scar/adapter/common', 0)
+    target_ssps['Unity_Clouds'] = (
+    ads_value + services_value + omid_value +
+    scar_v2000_value + scar_v2100_value + scar_v2300_value + scar_common_value
+    )
+    target_ssps.pop('Lcom/unity3d/ads', None)
+    target_ssps.pop('Lcom/unity3d/services', None)
+    target_ssps.pop('Lcom/iab/omid/library/unity3d', None)
+    target_ssps.pop('Lcom/unity3d/scar/adapter/v2000', None)
+    target_ssps.pop('Lcom/unity3d/scar/adapter/v2100', None)
+    target_ssps.pop('Lcom/unity3d/scar/adapter/v2300', None)
+    target_ssps.pop('Lcom/unity3d/scar/adapter/common', None)
+
+    # facebook 
+    facebook_ads_value = target_ssps.get('Lcom/facebook/ads', 0)
+    facebook_infer_value = target_ssps.get('Lcom/facebook/infer/annotation', 0)
+    target_ssps['facebook'] = facebook_ads_value + facebook_infer_value
+    target_ssps.pop('Lcom/facebook/ads', None)
+    target_ssps.pop('Lcom/facebook/infer/annotation', None)
+
+    # vungle
+    omid_vungle_value = target_ssps.get('Lcom/iab/omid/library/vungle', 0)
+    vungle_ads_value = target_ssps.get('Lcom/vungle/ads', 0)
+    target_ssps['vungle'] = omid_vungle_value + vungle_ads_value
+    target_ssps.pop('Lcom/iab/omid/library/vungle', None)
+    target_ssps.pop('Lcom/vungle/ads', None)
+
+    # digital/fiber
+    ignite_value = target_ssps.get('Lcom/digitalturbine/ignite', 0)
+    inneractive_value = target_ssps.get('Lcom/fyber/inneractive', 0)
+    marketplace_value = target_ssps.get('Lcom/fyber/marketplace', 0)
+    omid_fyber_value = target_ssps.get('Lcom/iab/omid/library/fyber', 0)
+    target_ssps['DigitalTurbine/Fyber'] = (
+    ignite_value + inneractive_value + marketplace_value + omid_fyber_value 
+    )
+    target_ssps.pop('Lcom/digitalturbine/ignite', None)
+    target_ssps.pop('Lcom/fyber/inneractive', None)
+    target_ssps.pop('Lcom/fyber/marketplace', None)
+    target_ssps.pop('Lcom/iab/omid/library/fyber', None)
+
+
     ssps = {
         "Lcom/google/ads/mediation/": [],
         "Lcom/applovin/mediation/ads/": [],
         "Lcom/ironsource/adapters/": [],
     }
     
-    print("7")
     with ZipFile(file_path, 'r') as apk_zip:
             for ssp in ssps.keys():
                 for dex_file in apk_zip.namelist():
@@ -422,8 +510,6 @@ def upload_apk():
                                         if medaited_ssp not in ssps[ssp] and not medaited_ssp.endswith(';'):
                                             ssps[ssp].append(medaited_ssp)
 
-    
-    print("8")
     return jsonify({
         "message": f"File '{file.filename}' successfully uploaded!",
         "target_ssps": target_ssps,
@@ -437,5 +523,5 @@ def findSSP():
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=4000, debug=True)
-    app.wsgi_app = ProxyFix(app.wsgi_app)  # This is optional, used when behind a proxy like Nginx
+    app.wsgi_app = ProxyFix(app.wsgi_app)  
     run_simple('0.0.0.0', 4000, app, use_reloader=True, threaded=True)
